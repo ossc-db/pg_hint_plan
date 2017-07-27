70f2c6a9bd4e121d9d106bb24aca727ca883cf23
Kyotaro Horiguchi <horiguchi.kyotaro@lab.ntt.co.jp>
2017-07-27 16:49:29 +0900

    Change version to 1.2.2
    1.2.1 has an assertion failure bug raised by DECLARE CURSOR with
    enabling table hint. Fixed it.

---
+++ pg_hint_plan--1.2.1--1.2.2.sql
@@ -0,0 +1,6 @@
+/* pg_hint_plan/pg_hint_plan--1.2.1--1.2.2.sql */
+
+-- complain if script is sourced in psql, rather than via CREATE EXTENSION
+\echo Use "CREATE EXTENSION pg_hint_plan" to load this file. \quit
+
+-- nothing to do upgrading from 1.2.1.
