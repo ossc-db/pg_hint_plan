8a9ac56f938d0fcec815daa705a11eebe0199068
Kyotaro Horiguchi <horikyoga.ntt@gmail.com>
2020-10-29 19:57:45 +0900

    Stabilize regression test.
    I was annoyed by frequent nonsubstantial change of title line length
    of explain output.  This commit makes maskout.sh replace the title
    lines with fixed content.

--- sql/maskout.sh
+++ sql/maskout.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 cat $1 | \
-sed 's/cost=10\{7\}[\.0-9]\+ /cost={inf}..{inf} /;s/cost=[\.0-9]\+ /cost=xxx..xxx /;s/width=[0-9]\+\([^0-9]\)/width=xxx\1/' |\
+sed 's/cost=10\{7\}[\.0-9]\+ /cost={inf}..{inf} /;s/cost=[\.0-9]\+ /cost=xxx..xxx /;s/width=[0-9]\+\([^0-9]\)/width=xxx\1/;s/^ *QUERY PLAN *$/  QUERY PLAN/;s/^--*$/----------------/' |\
 egrep -v "^ *((Planning time|JIT|Functions|Options):|\([0-9]* rows\))"
