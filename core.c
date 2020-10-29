874bc3a8efc984ab9d73e7996b2dcd7ca949b4f0
Kyotaro Horiguchi <horikyoga.ntt@gmail.com>
2020-10-29 13:15:25 +0900

    Update core.c and make_join_rel.c.
    From this version core.c and make_join_rel.c are automatically
    generated from core code.  Adjust them to auto-generated content.

--- core.c
+++ core.c
@@ -12,17 +12,17 @@
  *
  * src/backend/optimizer/path/allpaths.c
  *
- *	static functions:
- *	   set_plain_rel_pathlist()
- *     add_paths_to_append_rel()
- *     try_partitionwise_join()
- *
  *  public functions:
  *     standard_join_search(): This funcion is not static. The reason for
  *        including this function is make_rels_by_clause_joins. In order to
  *        avoid generating apparently unwanted join combination, we decided to
  *        change the behavior of make_join_rel, which is called under this
  *        function.
+ *
+ *	static functions:
+ *	   set_plain_rel_pathlist()
+ *	   set_append_rel_pathlist()
+ *	   create_plain_partial_paths()
  *
  * src/backend/optimizer/path/joinrels.c
  *
@@ -36,9 +36,8 @@
  *     join_is_legal()
  *     has_join_restriction()
  *     restriction_is_constant_false()
- *     update_child_rel_info()
  *     build_child_join_sjinfo()
- *
+ *     try_partitionwise_join()
  *
  * Portions Copyright (c) 1996-2020, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
@@ -47,8 +46,8 @@
  */
 
 static void populate_joinrel_with_paths(PlannerInfo *root, RelOptInfo *rel1,
-								RelOptInfo *rel2, RelOptInfo *joinrel,
-								SpecialJoinInfo *sjinfo, List *restrictlist);
+										RelOptInfo *rel2, RelOptInfo *joinrel,
+										SpecialJoinInfo *sjinfo, List *restrictlist);
 
 /*
  * set_plain_rel_pathlist
@@ -263,6 +262,7 @@ standard_join_search(PlannerInfo *root, int levels_needed, List *initial_rels)
 
 	return rel;
 }
+
 
 /*
  * create_plain_partial_paths
@@ -936,6 +936,7 @@ has_join_restriction(PlannerInfo *root, RelOptInfo *rel)
 	return false;
 }
 
+
 /*
  * restriction_is_constant_false --- is a restrictlist just FALSE?
  *
@@ -982,6 +983,7 @@ restriction_is_constant_false(List *restrictlist,
 	return false;
 }
 
+
 /*
  * Construct the SpecialJoinInfo for a child-join by translating
  * SpecialJoinInfo for the join between parents. left_relids and right_relids
@@ -1023,6 +1025,7 @@ build_child_join_sjinfo(PlannerInfo *root, SpecialJoinInfo *parent_sjinfo,
 
 	return sjinfo;
 }
+
 
 /*
  * Assess whether join between given two partitioned relations can be broken
