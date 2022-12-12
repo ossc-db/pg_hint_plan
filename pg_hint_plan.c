df963254b08d036cfd49e7da898b5c2f20aff0f0
Michael Paquier <michael@paquier.xyz>
2022-12-12 09:53:12 +0900

    Fix a few whitespaces in the code
    Issues with the style of the code spotted while applying automatically
    some checks onto it.  This change has no consequence for the code.
    
    Backpatch down to 10, per discusion with Julien Rouhaud.

--- pg_hint_plan.c
+++ pg_hint_plan.c
@@ -1717,7 +1717,7 @@ parse_hints(HintState *hstate, Query *parse, const char *str)
 }
 
 
-/* 
+/*
  * Get hints from table by client-supplied query string and application name.
  */
 static const char *
@@ -1749,9 +1749,9 @@ get_hints_from_table(const char *client_query, const char *client_application)
 			PushActiveSnapshot(GetTransactionSnapshot());
 			snapshot_set = true;
 		}
-	
+
 		SPI_connect();
-	
+
 		if (plan == NULL)
 		{
 			SPIPlanPtr	p;
@@ -1759,18 +1759,18 @@ get_hints_from_table(const char *client_query, const char *client_application)
 			plan = SPI_saveplan(p);
 			SPI_freeplan(p);
 		}
-	
+
 		qry = cstring_to_text(client_query);
 		app = cstring_to_text(client_application);
 		values[0] = PointerGetDatum(qry);
 		values[1] = PointerGetDatum(app);
-	
+
 		SPI_execute_plan(plan, values, nulls, true, 1);
-	
+
 		if (SPI_processed > 0)
 		{
 			char	*buf;
-	
+
 			hints = SPI_getvalue(SPI_tuptable->vals[0],
 								 SPI_tuptable->tupdesc, 1);
 			/*
@@ -1783,7 +1783,7 @@ get_hints_from_table(const char *client_query, const char *client_application)
 			strcpy(buf, hints);
 			hints = buf;
 		}
-	
+
 		SPI_finish();
 
 		if (snapshot_set)
@@ -1843,7 +1843,7 @@ get_query_string(ParseState *pstate, Query *query, Query **jumblequery)
 		if (IsA(target_query, ExplainStmt))
 		{
 			ExplainStmt *stmt = (ExplainStmt *)target_query;
-			
+
 			Assert(IsA(stmt->query, Query));
 			target_query = (Query *)stmt->query;
 
@@ -1904,7 +1904,7 @@ get_query_string(ParseState *pstate, Query *query, Query **jumblequery)
 				target_query = NULL;
 			}
 		}
-			
+
 		/* JumbleQuery accespts only a non-utility Query */
 		if (target_query &&
 			(!IsA(target_query, Query) ||
@@ -2391,7 +2391,7 @@ RowsHintParse(RowsHint *hint, HintState *hstate, Query *parse,
 
 		return str;
 	}
-	
+
 
 	/*
 	 * Transform relation names from list to array to sort them with qsort
@@ -2490,7 +2490,7 @@ ParallelHintParse(ParallelHint *hint, HintState *hstate, Query *parse,
 	}
 
 	hint->relname = linitial(name_list);
-		
+
 	/* The second parameter is number of workers */
 	hint->nworkers_str = list_nth(name_list, 1);
 	nworkers = strtod(hint->nworkers_str, &end_ptr);
@@ -2886,7 +2886,7 @@ get_current_hint_string(ParseState *pstate, Query *query)
 			pfree((void *)current_hint_str);
 			current_hint_str = NULL;
 		}
-		
+
 		if (jumblequery)
 		{
 			/*
@@ -3056,7 +3056,7 @@ pg_hint_plan_planner(Query *parse, int cursorOptions, ParamListInfo boundParams)
 	const char 	   *prev_hint_str = NULL;
 
 	/*
-	 * Use standard planner if pg_hint_plan is disabled or current nesting 
+	 * Use standard planner if pg_hint_plan is disabled or current nesting
 	 * depth is nesting depth of SPI calls. Other hook functions try to change
 	 * plan with current_hint_state if any, so set it to NULL.
 	 */
@@ -3106,7 +3106,7 @@ pg_hint_plan_planner(Query *parse, int cursorOptions, ParamListInfo boundParams)
 	/* run standard planner if the statement has not valid hint */
 	if (!hstate)
 		goto standard_planner_proc;
-	
+
 	/*
 	 * Push new hint struct to the hint stack to disable previous hint context.
 	 */
@@ -3119,7 +3119,7 @@ pg_hint_plan_planner(Query *parse, int cursorOptions, ParamListInfo boundParams)
 	setup_guc_enforcement(current_hint_state->set_hints,
 						   current_hint_state->num_hints[HINT_TYPE_SET],
 						   current_hint_state->context);
-	
+
 	current_hint_state->init_scan_mask = get_current_scan_mask();
 	current_hint_state->init_join_mask = get_current_join_mask();
 	current_hint_state->init_min_para_tablescan_size =
@@ -3142,7 +3142,7 @@ pg_hint_plan_planner(Query *parse, int cursorOptions, ParamListInfo boundParams)
 	{
 		ereport(pg_hint_plan_debug_message_level,
 				(errhidestmt(msgqno != qno),
-				 errmsg("pg_hint_plan%s: planner", qnostr))); 
+				 errmsg("pg_hint_plan%s: planner", qnostr)));
 		msgqno = qno;
 	}
 
@@ -3154,7 +3154,7 @@ pg_hint_plan_planner(Query *parse, int cursorOptions, ParamListInfo boundParams)
 	recurse_level++;
 	prev_hint_str = current_hint_str;
 	current_hint_str = NULL;
-	
+
 	/*
 	 * Use PG_TRY mechanism to recover GUC parameters and current_hint_state to
 	 * the state when this planner started when error occurred in planner.
@@ -3322,7 +3322,7 @@ find_parallel_hint(PlannerInfo *root, Index relid)
 
 	/*
 	 * Parallel planning is appliable only on base relation, which has
-	 * RelOptInfo. 
+	 * RelOptInfo.
 	 */
 	if (!rel)
 		return NULL;
@@ -3641,7 +3641,7 @@ restrict_indexes(PlannerInfo *root, ScanMethodHint *hint, RelOptInfo *rel,
 			disprelname = get_rel_name(rte->relid);
 		else
 			disprelname = hint->relname;
-			
+
 
 		initStringInfo(&rel_buf);
 		quote_value(&rel_buf, disprelname);
@@ -3656,7 +3656,7 @@ restrict_indexes(PlannerInfo *root, ScanMethodHint *hint, RelOptInfo *rel,
 	}
 }
 
-/* 
+/*
  * Return information of index definition.
  */
 static ParentIndexInfo *
@@ -3840,7 +3840,7 @@ setup_hint_enforcement(PlannerInfo *root, RelOptInfo *rel,
 		 * redundant setup cost.
 		 */
 		current_hint_state->parent_relid = new_parent_relid;
-				
+
 		/* Find hints for the parent */
 		current_hint_state->parent_scan_hint =
 			find_scan_hint(root, current_hint_state->parent_relid);
@@ -4582,7 +4582,7 @@ pg_hint_plan_join_search(PlannerInfo *root, int levels_needed,
 	{
 		ListCell   *lc;
 		int 		nworkers = 0;
-	
+
 		foreach (lc, initial_rels)
 		{
 			ListCell *lcp;
