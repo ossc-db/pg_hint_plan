be6d36393cbea4a1bc86bb30bc4312b5407796f9
Michael Paquier <michael@paquier.xyz>
2023-01-20 15:02:42 +0900

    Version 1.3.8

--- sql/oldextversions.sql
+++ sql/oldextversions.sql
@@ -18,4 +18,6 @@ ALTER EXTENSION pg_hint_plan UPDATE TO "1.3.6";
 \dx+ pg_hint_plan
 ALTER EXTENSION pg_hint_plan UPDATE TO "1.3.7";
 \dx+ pg_hint_plan
+ALTER EXTENSION pg_hint_plan UPDATE TO "1.3.8";
+\dx+ pg_hint_plan
 DROP EXTENSION pg_hint_plan;
