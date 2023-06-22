a37b6ba1a61c5d85d71f36b2eb92470803a9e30b
Michael Paquier <michael@paquier.xyz>
2023-06-22 09:52:42 +0900

    Fix typos in comments
    Per pull request #135, from Mr Dk.
    
    Backpatch-through: 11

--- pg_hint_plan.c
+++ pg_hint_plan.c
@@ -2547,7 +2547,7 @@ get_current_join_mask()
 }
 
 /*
- * Sets GUC prameters without throwing exception. Reutrns false if something
+ * Sets GUC parameters without throwing exception. Returns false if something
  * wrong.
  */
 static int
@@ -2933,7 +2933,7 @@ get_current_hint_string(Query *query, const char *query_str,
 			msgqno = qno;
 		}
 
-		/* retrun if we have hint string here */
+		/* return if we have hint string here */
 		if (current_hint_str)
 			return;
 	}
