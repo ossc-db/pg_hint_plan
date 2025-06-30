b55ac01793db777cb74f001e34c8cf48ad6e9519
Michael Paquier <michael@paquier.xyz>
2025-06-30 10:39:00 +0900

    Version 1.6.2
    The release is not ready yet, but this does all the ground work to be
    able to do a release at any moment.

--- pg_hint_plan--1.4.3--1.4.4.sql
+++ pg_hint_plan--1.4.3--1.4.4.sql
@@ -1,7 +1,7 @@
-/* pg_hint_plan/pg_hint_plan--1.3.10--1.4.sql */
+/* pg_hint_plan/pg_hint_plan--1.4.3--1.4.4.sql */
 
 -- complain if script is sourced in psql, rather than via ALTER EXTENSION
-\echo Use "ALTER EXTENSION pg_hint_plan UPDATE TO '1.4'" to load this file. \quit
+\echo Use "ALTER EXTENSION pg_hint_plan UPDATE TO '1.4.4'" to load this file. \quit
 
 SELECT pg_catalog.pg_extension_config_dump('hint_plan.hints','');
 SELECT pg_catalog.pg_extension_config_dump('hint_plan.hints_id_seq','');
